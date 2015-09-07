486e20d40e4ad4112236d556501fb40dc62c712e
marmusevich <myrmik@gmail.com>
2015-09-07 08:36:11 +0300

    +

---
+++ WorckWithCadastr_V6/ArcButtons/arcBtn_Open_.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+
+namespace WorckWithCadastr_V6
+{
+    public class arcBtn_Open_ArhivDocument : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public arcBtn_Open_ArhivDocument()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            ArcMap.Application.CurrentTool = null;
+            //frm.ShowForView();
+        }
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+    }
+}
